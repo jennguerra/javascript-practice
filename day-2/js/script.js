3a9012136bf400560bf000e505013ca0513e12b8
Jenn Guerra <valiumisdead@yahoo.com>
2021-01-22 12:52:52 -0800

    added day-2 and day-3 to main

---
+++ day-2/js/script.js
@@ -0,0 +1,20 @@
+console.log(1);
+
+// this is my single line comment
+
+
+
+/* this is my multi line comment */
+
+
+console.log("Jenn Guerra");
+
+console.log("chocolate");
+
+console.log("Supercalifragilisticexpialidocious".length);
+
+console.log(7n);
+
+console.log(typeof"Supercalifragilisticexpialidocious".length);
+
+console.log(typeof 7n);
